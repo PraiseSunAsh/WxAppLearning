bc0e2d337e8449e8e2d21d64703287383acf71ef
Jasonlee <690163223@qq.com>
2019-06-05 16:53:49 +0800

    yun

---
+++ 机械学霸v0.1/miniprogram/pages/video_detail/video_detail.js
@@ -0,0 +1,72 @@
+// pages/video_detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    link:"",
+    title:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var appInst =  getApp();
+    var linkValue = appInst.globalData.link;
+    //在加载页面的时候把链接和标题传过去
+    this.setData({link:linkValue});
+    var titleValue = appInst.globalData.title;
+    this.setData({title:titleValue});
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
